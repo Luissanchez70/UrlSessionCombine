b7a74fd5796afc1258e21c8e4a594e90b2f3ec67
Luis <luis.fernandomunoz@plexus.es>
2024-05-09 10:06:38 +0200

    Initial Commit

---
+++ UrlSessionCombine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrlSessionCombine
+//
+//  Created by Luis Fernando Sanchez Muñoz on 9/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UrlSessionCombineUITests/UrlSessionCombineUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UrlSessionCombineUITestsLaunchTests.swift
+//  UrlSessionCombineUITests
+//
+//  Created by Luis Fernando Sanchez Muñoz on 9/5/24.
+//
+
+import XCTest
+
+final class UrlSessionCombineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
